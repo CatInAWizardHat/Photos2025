dce5b7e4abb779047307bee0cbcdc14a167fc2d6
Alex <515260@camosun>
2025-02-26 14:00:19 -0800

    Added photo class

---
+++ Photos2025/Photo.swift
@@ -0,0 +1,21 @@
+//
+//  Photo.swift
+//  Photos2025
+//
+//  Created by IMD 224 on 2025-02-26.
+//
+import UIKit
+import Observation
+
+@Observable
+class Photo {
+    var image: UIImage
+    var name: String
+    var date: Date
+    
+    init(image: UIImage, name: String, date: Date) {
+        self.image = image
+        self.name = name
+        self.date = date
+    }
+}
