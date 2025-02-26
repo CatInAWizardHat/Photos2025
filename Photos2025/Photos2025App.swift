ac5341db9b18bfb6af1311d57a3cf3ee3dad1efe
Alex <515260@camosun>
2025-02-26 13:51:14 -0800

    Initial Commit

---
+++ Photos2025/Photos2025App.swift
@@ -0,0 +1,17 @@
+//
+//  Photos2025App.swift
+//  Photos2025
+//
+//  Created by IMD 224 on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct Photos2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Photos2025Tests/Photos2025Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Photos2025Tests.swift
+//  Photos2025Tests
+//
+//  Created by IMD 224 on 2025-02-26.
+//
+
+import Testing
+@testable import Photos2025
+
+struct Photos2025Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
