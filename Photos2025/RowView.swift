42d7775dfa244ea627f7b5ecc976c3bb548075a7
Alex <515260@camosun>
2025-02-26 14:56:01 -0800

    Updated RowView

---
+++ Photos2025/RowView.swift
@@ -0,0 +1,18 @@
+//
+//  RowView.swift
+//  Photos2025
+//
+//  Created by IMD 224 on 2025-02-26.
+//
+
+import SwiftUI
+
+struct RowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RowView()
+}
